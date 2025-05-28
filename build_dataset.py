9c718b4035bdf68bd7f7c36afac538fa365f8f2c
ryanpdemilt <demilt.ryan@gmail.com>
2025-05-28 16:28:41 -0400

    test config

--- build_dataset.py
+++ build_dataset.py
@@ -20,7 +20,18 @@ end_year = int(args.endyear)
 years = [ str(start_year +i ) for i in range(end_year - start_year + 1)] 
 start_day = 1
 end_day = 31
-days = [ str(start_day +i ).zfill(2) for i in range(end_day - start_day + 1)]
+# days = [ str(start_day +i ).zfill(2) for i in range(end_day - start_day + 1)]
+
+# times = [
+#             '00:00', '01:00', '02:00',
+#             '03:00', '04:00', '05:00',
+#             '06:00', '07:00', '08:00',
+#             '09:00', '10:00', '11:00',
+#             '12:00', '13:00', '14:00',
+#             '15:00', '16:00', '17:00',
+#             '18:00', '19:00', '20:00',
+#             '21:00', '22:00', '23:00',
+#     ]
 
 variable_list = [
         "10m_u_component_of_wind",
@@ -39,10 +50,13 @@ months = ['01', '02', '03',
                     '04', '05', '06',
                     '07', '08', '09',
                     '10', '11', '12']
+days = ['15']
+times = ['12:00']
+
 
 for year in years:
     for month in months:
-        downloaded_file = f'~/mnt/eofm-benchmark/data/climatedownscaling/ERA5-hourly-{year}-{month}.zip'
+        downloaded_file = f'~/mnt/eofm-benchmark/data/climatedownscaling/test_ERA5-hourly-{year}-{month}.zip'
 
         request = {
             'product_type':['reanalysis'],
@@ -50,16 +64,7 @@ for year in years:
             'year':[year],
             'month': [month],
             'day':days,
-            'time': [
-                    '00:00', '01:00', '02:00',
-                    '03:00', '04:00', '05:00',
-                    '06:00', '07:00', '08:00',
-                    '09:00', '10:00', '11:00',
-                    '12:00', '13:00', '14:00',
-                    '15:00', '16:00', '17:00',
-                    '18:00', '19:00', '20:00',
-                    '21:00', '22:00', '23:00',
-                ],
+            'time': times,
             'data_format':file_format,
             'download_format':download_format
         }
