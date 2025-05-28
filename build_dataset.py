ad893b73decc9675e07dec2013be0691dd81a856
ryanpdemilt <demilt.ryan@gmail.com>
2025-05-28 13:36:55 -0400

    request chunking

--- build_dataset.py
+++ build_dataset.py
@@ -35,32 +35,35 @@ variable_list = [
 file_format = 'netcdf'
 download_format = 'zip'
 
-for year in years:
-    downloaded_file = f'mnt/eofm-benchmark/data/climatedownscaling/ERA5-hourly-{year}.zip'
-
-    request = {
-        'product_type':['reanalysis'],
-        'variable':variable_list,
-        'year':[year],
-        'day':days,
-        'month': ['01', '02', '03',
+months = ['01', '02', '03',
                     '04', '05', '06',
                     '07', '08', '09',
-                    '10', '11', '12'],
-        'time': [
-                '00:00', '01:00', '02:00',
-                '03:00', '04:00', '05:00',
-                '06:00', '07:00', '08:00',
-                '09:00', '10:00', '11:00',
-                '12:00', '13:00', '14:00',
-                '15:00', '16:00', '17:00',
-                '18:00', '19:00', '20:00',
-                '21:00', '22:00', '23:00',
-            ],
-        'data_format':file_format,
-        'download_format':download_format
-    }
+                    '10', '11', '12']
 
-    client = cdsapi.Client()
+for year in years:
+    for month in month:
+        downloaded_file = f'mnt/eofm-benchmark/data/climatedownscaling/ERA5-hourly-{year}-{month}.zip'
 
-    client.retrieve(dataset,request,downloaded_file)
+        request = {
+            'product_type':['reanalysis'],
+            'variable':variable_list,
+            'year':[year],
+            'month': [month],
+            'day':days,
+            'time': [
+                    '00:00', '01:00', '02:00',
+                    '03:00', '04:00', '05:00',
+                    '06:00', '07:00', '08:00',
+                    '09:00', '10:00', '11:00',
+                    '12:00', '13:00', '14:00',
+                    '15:00', '16:00', '17:00',
+                    '18:00', '19:00', '20:00',
+                    '21:00', '22:00', '23:00',
+                ],
+            'data_format':file_format,
+            'download_format':download_format
+        }
+
+        client = cdsapi.Client()
+
+        client.retrieve(dataset,request,downloaded_file)
