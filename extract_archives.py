25e5f577f6d1165baf4a8c4e199ef099ce16dee1
ryanpdemilt <demilt.ryan@gmail.com>
2025-06-03 11:22:08 -0400

    archive extraction/cleanup

---
+++ extract_archives.py
@@ -0,0 +1,37 @@
+import os
+import glob
+import zipfile
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--startyear',type=str,required=False,default='2016')
+parser.add_argument('--endyear',type=str,required=False,default='2017')
+parser.add_argument('--path',type=str,required=False,default='/home/rdemilt/mnt/eofm-benchmark/climatedownscaling')
+parser.add_argument('--dest',type=str,required=False,default='/home/rdemilt/mnt/eofm-benchmark/climatedownscaling/era5')
+
+args = parser.parse_args()
+
+path = args.path
+destination = args.dest
+
+start_year = args.startyear
+end_year = args.endyear
+years = [ str(start_year +i ) for i in range(end_year - start_year + 1)] 
+
+
+def extract_zip(zip_path,destination_path):
+    with zipfile.ZipFile(zip_path,'r') as zip_ref:
+        zip_ref.extractall(destination_path)
+    print(f'Successfully extracted file: {zip_path}')
+
+
+
+for year in years:
+    archives = glob.glob(os.path.join(path,f'*{year}*.zip'))
+    year_folder = os.path.join(destination,year)
+    if not os.path.exists(year_folder):
+        os.mkdir(year_folder)
+
+    for archive in archives:
+        extract_zip(archive,year_folder)
