ed791fb52237b7a271c007125aa1980e8c9d4fde
Ryan <rdemilt@sig-gis.com>
2025-06-13 14:50:48 +0000

    rtma data proccessing

--- rtma_gee_processing.py
+++ rtma_gee_processing.py
@@ -9,9 +9,20 @@ california_lat_max = 42.2
 california_lon_min = -124.7
 california_lon_max = -113.9
 
+date_start = '2020-01-01'
+date_end = '2020-01-02'
+
 california_bbox = ee.Geometry.BBox(california_lon_min,california_lat_min,california_lon_max,california_lat_max)
 
-dataset = ee.ImageCollection('NOAA/NWS/RTMA').filter(ee.Filter.date('2017-01-01','2018-01-01'))
+dataset = ee.ImageCollection('NOAA/NWS/RTMA').filter(ee.Filter.date(date_start,date_end))
 dataset = dataset.filter(ee.Filter.bounds(california_bbox))
 
-variables = dataset.select(['UGRD','VGRD','TMP','GUST','ACPC01'])
+variables = dataset.select(['UGRD','VGRD','TMP','GUST','ACPC01','HGT'])
+
+
+request = {
+    'expression': variables,
+    'fileFormat':'GEOPANDAS_GEODATAFRAME'
+}
+
+data = ee.data.computePixels(request)
